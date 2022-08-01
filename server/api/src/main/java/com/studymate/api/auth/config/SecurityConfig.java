be584d5ce1df3b346efb93b9be77a877df708378
veryneuron <veryneuron1@gmail.com>
2022-08-02 00:32:03 +0900

    스프링 시큐리티 config 컴포넌트로 변경

--- server/api/src/main/java/com/studymate/api/auth/config/SecurityConfig.java
+++ server/api/src/main/java/com/studymate/api/auth/config/SecurityConfig.java
@@ -3,27 +3,37 @@ package com.studymate.api.auth.config;
 import com.studymate.api.auth.jwt.JwtTokenFilter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.ExceptionTranslationFilter;
 
 @Slf4j
-@EnableWebSecurity(debug = true)
+@Configuration
 @RequiredArgsConstructor
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
     private final JwtTokenFilter jwtTokenFilter;
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .httpBasic().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                        .authorizeRequests().antMatchers("/", "/api/auth").permitAll()
-                        .anyRequest().authenticated();
-        http.addFilterBefore(jwtTokenFilter, ExceptionTranslationFilter.class);
+                .authorizeRequests().antMatchers("/api/auth/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilterBefore(jwtTokenFilter, ExceptionTranslationFilter.class);
+        return http.build();
     }
 }
