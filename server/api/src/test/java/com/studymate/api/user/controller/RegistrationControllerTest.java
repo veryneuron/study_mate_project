1b4547bd5e94c1d444c24b1fb111ca35b22b2d92
veryneuron <veryneuron@naver.com>
2022-08-08 12:02:55 +0900

    rabbitmq auto startup 테스트용 프로필 생성

--- server/api/src/test/java/com/studymate/api/user/controller/RegistrationControllerTest.java
+++ server/api/src/test/java/com/studymate/api/user/controller/RegistrationControllerTest.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import javax.transaction.Transactional;
@@ -31,6 +32,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 @Transactional
+@ActiveProfiles("test")
 class RegistrationControllerTest {
     //Integration Test
     @Autowired
