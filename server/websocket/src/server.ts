8dc87e0eda49562861e588e28b6041b07fb0216f
veryneuron <veryneuron@naver.com>
2022-08-10 23:16:00 +0900

    mqtt 클라이언트 및 웹소켓 서버 설정

--- server/websocket/src/server.ts
+++ server/websocket/src/server.ts
@@ -1,3 +1,14 @@
-export function sum(a: number, b: number): number {
-  return a + b;
-}
+import { WebSocketServer } from 'ws';
+import { connectToDatabase } from './database/chattingService';
+import { connect } from 'mqtt';
+import { init } from './init';
+
+connectToDatabase().catch((err) => {
+  console.log(err);
+  process.exit();
+});
+
+const wss = new WebSocketServer({ port: 8080 });
+const client = connect('mqtt://localhost:1996');
+
+init(wss, client);
