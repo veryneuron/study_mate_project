910ebba1c4c4f32c26d2c189b6aa4dbf9222b5ea
veryneuron <veryneuron@gmail.com>
2022-11-19 22:55:47 +0900

    이미지 표시 삭제 및 status 체크 추가

--- raspberry_pi/eyetracking/main.py
+++ raspberry_pi/eyetracking/main.py
@@ -16,15 +16,17 @@ global count_right
 global avg_sclera_left
 global avg_sclera_right
 
+
 def send_msg(client, topic, msg):
     client.publish(topic, msg, 1)
 
-#눈동자 마스킹
+
+# 눈동자 마스킹
 def eye_position(shape, gray, left, right):
-    #threshold 값
+    # threshold 값
     thresh_value = 80
 
-    #동공 마스킹
+    # 동공 마스킹
     mask = np.zeros(gray.shape[:2], dtype=np.uint8)
     cv2.fillPoly(mask, [left], 255)
     cv2.fillPoly(mask, [right], 255)
@@ -35,7 +37,7 @@ def eye_position(shape, gray, left, right):
     return _thresh
 
 
-#흰자영역 평균크기 계산
+# 흰자영역 평균크기 계산
 def calcul_sclera_avg(thresh, mid, right=False):
     global avg_sclera_left
     global avg_sclera_right
@@ -43,11 +45,11 @@ def calcul_sclera_avg(thresh, mid, right=False):
     global count_left
     global count_right
 
-    #눈 중앙 기준 흰자 영역 왼쪽 오른쪽 contouring
+    # 눈 중앙 기준 흰자 영역 왼쪽 오른쪽 contouring
     cnts_left, _ = cv2.findContours(thresh[:, 0:mid], cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     cnts_right, _ = cv2.findContours(thresh[:, mid:], cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
-    #contourArea 큰 영역의 반대 방향이 응시 방향
+    # contourArea 큰 영역의 반대 방향이 응시 방향
     if len(cnts_left) != 0 and len(cnts_right) != 0:
 
         cnt_left = max(cnts_left, key=cv2.contourArea)
@@ -82,14 +84,14 @@ def calcul_sclera_avg(thresh, mid, right=False):
                 print("left done")
 
 
-#바라보는방향
+# 바라보는방향
 def gaze_check(gray, thresh, mid, ratio, right=False):
     # 눈동자 움직임 민감도
     # 0: 좌측 1 : 우측
     # left, right : 흰자영역
     sensitivity_left = np.array([35, 35])
     sensitivity_right = np.array([35, 35])
-    #눈감음 민감도
+    # 눈감음 민감도
     sensitivity_closed = 12
 
     ratio = 1 + ratio
@@ -97,22 +99,22 @@ def gaze_check(gray, thresh, mid, ratio, right=False):
     area_left = 0
     area_right = 0
 
-    #흰자 평균값
+    # 흰자 평균값
     global avg_sclera_left
     global avg_sclera_right
 
     global count_left
     global count_right
 
-    #눈 중앙 기준 흰자 영역 왼쪽 오른쪽 contouring
+    # 눈 중앙 기준 흰자 영역 왼쪽 오른쪽 contouring
     cnts_left, _ = cv2.findContours(thresh[:, 0:mid], cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     cnts_right, _ = cv2.findContours(thresh[:, mid:], cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
-    #contourArea 큰 영역의 반대 방향이 응시 방향
+    # contourArea 큰 영역의 반대 방향이 응시 방향
     if len(cnts_left) != 0 and len(cnts_right) != 0:
 
-        #cv2.drawContours(gray, cnts_left, 0, (0, 0, 255), 2)
-        #cv2.drawContours(gray, cnts_right, 0, (0, 0, 255), 2)
+        # cv2.drawContours(gray, cnts_left, 0, (0, 0, 255), 2)
+        # cv2.drawContours(gray, cnts_right, 0, (0, 0, 255), 2)
 
         cnt_left = max(cnts_left, key=cv2.contourArea)
         cnt_right = max(cnts_right, key=cv2.contourArea)
@@ -120,8 +122,8 @@ def gaze_check(gray, thresh, mid, ratio, right=False):
         area_left = cv2.contourArea(cnt_left)
         area_right = cv2.contourArea(cnt_right)
 
-        #print("area_left : " + str(area_left))
-        #print("area_right : " + str(area_right))
+        # print("area_left : " + str(area_left))
+        # print("area_right : " + str(area_right))
 
         # avg_sclera_left[0] : 왼쪽눈 왼쪽 흰자영역
         # avg_sclera_left[1] : 오른쪽눈 왼쪽 흰자영역
@@ -139,18 +141,18 @@ def gaze_check(gray, thresh, mid, ratio, right=False):
         eye_R_sclera_R_change = int(area_right - int(avg_sclera_right[1] * ratio))
         eye_R_sclera_R_base = ((avg_sclera_right[1] - sensitivity_right[1]) * ratio)
 
-        #왼쪽 오른쪽 움직임 avg에 따라 sensitivity 조절하는 방법으로 수정하기
+        # 왼쪽 오른쪽 움직임 avg에 따라 sensitivity 조절하는 방법으로 수정하기
         if right:
             # 왼쪽 쳐다봄 or 오른쪽 쳐다봄
             if eye_R_sclera_L_change > eye_R_sclera_L_base or eye_R_sclera_R_change > eye_R_sclera_R_base:
                 return 1
-        elif right==False:
-            #왼쪽 쳐다봄 or 오른쪽 쳐다봄
+        elif right == False:
+            # 왼쪽 쳐다봄 or 오른쪽 쳐다봄
             if eye_L_sclera_L_change > eye_L_sclera_L_base or eye_L_sclera_R_change > eye_L_sclera_R_base:
                 return 1
 
 
-#얼굴 크기 변화율 계산
+# 얼굴 크기 변화율 계산
 def calcul_ratio(face, base_length):
     face_left = face[0][0]
     face_right = face[1][0]
@@ -161,15 +163,17 @@ def calcul_ratio(face, base_length):
 
     return ratio
 
-#눈 감음 인식 (EAR 알고리즘)
+
+# 눈 감음 인식 (EAR 알고리즘)
 def eye_aspect_ratio(eye):
     A = distance.euclidean(eye[1], eye[5])
     B = distance.euclidean(eye[2], eye[4])
     C = distance.euclidean(eye[0], eye[3])
 
-    ear = (A+B) / (2.0 * C)
+    ear = (A + B) / (2.0 * C)
 
     return ear
+
 
 def main():
     global count_left
@@ -211,11 +215,11 @@ def main():
     base_length = 0
 
     ##mqtt연결
-    myMQTTClient = AWSIoTMQTTClient("pi_compute")
+    myMQTTClient = AWSIoTMQTTClient("pi_compute_test")
     myMQTTClient.configureEndpoint("a27cn38pezif4g-ats.iot.ap-northeast-1.amazonaws.com", 8883)
-    myMQTTClient.configureCredentials("/Users/lsy/Documents/untitled folder/aws_certificate/cert/RootCA.cer",
-                                      "/Users/lsy/Documents/untitled folder/aws_certificate/cert/private.pem.key",
-                                      "/Users/lsy/Documents/untitled folder/aws_certificate/cert/certificate.pem.crt")
+    myMQTTClient.configureCredentials("C:/Users/veryn/cert/AmazonRootCA1.pem",
+                                      "C:/Users/veryn/cert/5b91b57f7fea1b0872ed25103368129f3e9247f113e6f266ed69101a5d462800-private.pem.key",
+                                      "C:/Users/veryn/cert/5b91b57f7fea1b0872ed25103368129f3e9247f113e6f266ed69101a5d462800-certificate.pem.crt.txt")
 
     myMQTTClient.configureOfflinePublishQueueing(-1)
     myMQTTClient.configureDrainingFrequency(2)
@@ -227,14 +231,15 @@ def main():
 
     time.sleep(2)
 
-    cap = cv2.VideoCapture('rtsp://172.20.10.9:8555/unicast')
+    cap = cv2.VideoCapture('rtsp://192.168.47.157:8556/unicast')
 
     while True:
-        _, frame = cap.read()
+        status, frame = cap.read()
+        if not status:
+            continue
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         faces = detector(gray)
-        cv2.imshow("rtsp", gray)
-
+        # cv2.imshow("rtsp", gray)
         # 얼굴 인식
         for face in faces:
             face_count += 1
@@ -255,8 +260,8 @@ def main():
                               [shape.part(47).x, shape.part(47).y]], np.int32)
 
             head = np.array([[shape.part(0).x, shape.part(0).y],
-                            [shape.part(16).x, shape.part(16).y],
-                            [shape.part(30).x, shape.part(30).y]], np.int32)
+                             [shape.part(16).x, shape.part(16).y],
+                             [shape.part(30).x, shape.part(30).y]], np.int32)
 
             # 얼굴 범위 출력
             _thresh = eye_position(shape, gray, left, right)
@@ -265,6 +270,7 @@ def main():
 
             mid_left = (shape.part(36).x + shape.part(39).x) // 2
             mid_right = (shape.part(42).x + shape.part(45).x) // 2
+            print(count_left, count_right)
 
             if count_left < 11 or count_right < 11:
                 calcul_sclera_avg(_thresh[:, 0:mid], mid_left)
@@ -324,15 +330,13 @@ def main():
                         # 라즈베리파이로 종료 신호 전송
                         send_msg(myMQTTClient, "status/focused", "True")
 
-
                 temp = 0
-                cv2.imshow("thresh", _thresh)
+                # cv2.imshow("thresh", _thresh)
                 print("")
 
         if face_count == 0:
-            print("absence or turn")
+            # print("absence or turn")
             count_uncon += 1
-
 
         if start_time == 0 and count_left > 10 and count_right > 10 and face_count == 0 and count_uncon >= 30:
             start_time = int(time.time())
@@ -343,12 +347,13 @@ def main():
         face_count = 0
 
         key = cv2.waitKey(1)
-        #ESC 입력 시 종료
+        # ESC 입력 시 종료
         if key == 27:
             break
 
     cap.release()
     cv2.destroyAllWindows()
+
 
 if __name__ == "__main__":
     main()
